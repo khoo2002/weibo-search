0565164bffdcdc49d4633bc09bb79126b48c2160
khoozhaowei2002 <khoozhaowei2002@outlook.my>
2023-03-16 23:05:38 +0800

    commit

--- weibo/settings.py
+++ weibo/settings.py
@@ -12,7 +12,8 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'SCF=AmfW9QDqyGWBwLHyGOrFvhJm30tIJrsw8Qgy6WoNEKiHgzNMmXlp4UvBGLq_YhCzt4EvACknZpLwubcRu2AIO78.; SUB=_2A25JF1PLDeRhGeFK41UR8ibPyjSIHXVq-H2DrDV6PUNbktAGLWnckW1NQsy6QHl_ip-nGRiGvmzzScx-HprkgGVq; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWpq7FclDLxI-quPK.8H5x05JpX5KMhUgL.FoMX1hM7eon0eKn2dJLoI7_AIs8aUcHDIcyad7tt; SSOLoginState=1678975899; ALF=1681567899; _T_WM=349479652123394ae78232abd058af02',
+    #'cookie': 'WEIBOCN_FROM=1110006030; _T_WM=34488798411; SUB=_2A25JFi64DeRhGeNH71AQ9SrJzz2IHXVq-LLwrDV6PUJbktANLRGskW1NSqeLdVChM6mXdB6Q036lRa27icYFnmMq; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWJ7k20ZPOUJNq8o6rNs59y5NHD95Qf1KBEeK-XSKBpWs4DqcjsIcyadNDN; SSOLoginState=1678925544; XSRF-TOKEN=4c99db; MLOGIN=1; mweibo_short_token=53d8f3b544; M_WEIBOCN_PARAMS=luicode=20000174&uicode=20000174'
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,7 +25,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['抑郁']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,16 +34,16 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2022-07-01'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2022-07-31'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
 # 图片文件存储路径
-IMAGES_STORE = './'
+IMAGES_STORE = './resultDepression/image/'
 # 视频文件存储路径
-FILES_STORE = './'
+FILES_STORE = './resultDepression/video/'
 # 配置MongoDB数据库
 # MONGO_URI = 'localhost'
 # 配置MySQL数据库，以下为默认配置，可以根据实际情况更改，程序会自动生成一个名为weibo的数据库，如果想换其它名字请更改MYSQL_DATABASE值
